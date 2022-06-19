5b9d0824c3efc26e5d03748ace94f7acc4814d1e
Sagar4971 <sagarnerkar2912@gmail.com>
2022-06-19 16:33:46 +0530

    BLOG-8:create UserService Interface in services

---
+++ src/main/java/com/BikkadIT/BlogApplication/services/UserService.java
@@ -0,0 +1,19 @@
+package com.BikkadIT.BlogApplication.services;
+
+import java.util.List;
+
+import com.BikkadIT.BlogApplication.payloads.UserDto;
+
+public interface UserService {
+	
+	UserDto createUser(UserDto user);
+	
+	UserDto updateUser(UserDto user, Integer userId);
+	
+	UserDto getUserById(Integer userId);
+	
+	List<UserDto> getAllUsers();
+	
+	void deleteUser(Integer userId);
+
+}
